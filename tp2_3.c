9908c146f26e424edfde191b703e9525e1fab6a7
Luciano Torres <lucianotorreslanzavecchia@gmail.com>
2025-04-01 13:37:33 -0300

    punto 3 realizado

---
+++ tp2_3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 5
+#define M 7
+
+int main()
+{
+    srand(time(NULL));
+    int i,j;
+    float mt[N][M];
+    float *punt = &mt;
+    for(i = 0;i<N; i++)
+    {
+    for(j = 0;j<M; j++)
+    {
+    *(punt + i * M +j)= 1 + rand()%100;
+    printf(" %lf ", mt[i][j]);
+    }
+    printf("\n");
+    }
+return 0;
+}
