5047381b0cbbdc422b8f33caeba77ca009f8d2a8
Luciano Torres <lucianotorreslanzavecchia@gmail.com>
2025-04-01 13:43:53 -0300

    correccion archivo tp2_3.c

--- tp2_3.c
+++ tp2_3.c
@@ -9,13 +9,13 @@ int main()
     srand(time(NULL));
     int i,j;
     float mt[N][M];
-    float *punt = &mt;
+    float *punt = &mt[0][0];
     for(i = 0;i<N; i++)
     {
     for(j = 0;j<M; j++)
     {
-    *(punt + i * M +j)= 1 + rand()%100;
-    printf(" %lf ", mt[i][j]);
+    *(punt + (i * M +j))= 1 + rand()%100;
+    printf(" %lf", *(punt + (i * M +j)));
     }
     printf("\n");
     }
