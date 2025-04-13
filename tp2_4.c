b98288f7b1bf52c41dfdb9953b7b3ac0cbdff09b
Luciano Torres <lucianotorreslanzavecchia@gmail.com>
2025-04-13 16:50:35 -0300

    almacenamiento de los datos

--- tp2_4.c
+++ tp2_4.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define CANT 5
 struct compu {
     int velocidad; // Velocidad de procesamiento en GHz (valor entre 1 y 3)
     int anio; // Año de fabricación (valor entre 2015 y 2024)
@@ -13,12 +14,15 @@ int main()
 {
     srand(time(NULL));
     char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core","Pentium"};    
-    int i;    
-    i = rand() % 6;
-    struct compu pc;
-    pc.velocidad = (rand()% 3) + 1; //genera numeros aleatorios de 1 a 3
-    pc.anio = (rand() % (2024 - 2015 + 1)) + 2015;
-    pc.cantidad_nucleos = (rand()% 8) + 1;
-    pc.tipo_cpu = tipos[i];
-    return 0;
+    int i;
+    struct compu pc[CANT];
+    for (int j = 0; j < CANT; j++)
+    {
+        i = rand() % 6;
+        pc[j].velocidad = (rand()% 3) + 1; //genera numeros aleatorios de 1 a 3
+        pc[j].anio = (rand() % (2024 - 2015 + 1)) + 2015; //genera numeros entre 2015 y 2024
+        pc[j].cantidad_nucleos = (rand()% 8) + 1; //genera numeros entre 1 y 8
+        pc[j].tipo_cpu = tipos[i];
+    }
+return 0;
 }
